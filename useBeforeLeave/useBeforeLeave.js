49cd68ace5b25d32ddca2dec6631f34fd96a9145
hanjy-slowturtle <h920915@gmail.com>
2022-08-16 22:29:48 +0900

    ~ #2.4 useBeforeLeave

---
+++ useBeforeLeave/useBeforeLeave.js
@@ -0,0 +1,18 @@
+export const useBeforeLeave = (onBefore) => {
+  if (onBefore && typeof onBefore != "function") {
+    return;
+  }
+  useEffect(() => {
+    const handle = (event) => {
+      const { clientY } = event;
+      if (clientY <= 0) {
+        console.log("leaving");
+        onBefore && onBefore();
+      }
+    };
+    document.addEventListener("mouseleave", handle);
+    return () => {
+      document.removeEventListener("mouseleave", handle);
+    };
+  }, []);
+};
